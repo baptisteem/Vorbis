f4960e7c657e47f4ef5d6b1e95f74ceb03ec0755
ensimag <ensimag@ensilinux.(none)>
2014-06-04 10:36:44 +0200

    time_domain done mais temps très long :/

--- Makefile
+++ Makefile
@@ -20,26 +20,28 @@ OBJECTS   = $(OBJDIR)/main.o                                                  \
             $(OBJDIR)/floor.o $(OBJDIR)/floor0.o $(OBJDIR)/floor1.o           \
             $(OBJDIR)/vorbis_main.o                                           \
             $(OBJDIR)/vorbis_packet.o                                         \
-            $(OBJDIR)/vorbis_io.o                                             \
-            $(OBJDIR)/header3.o                                               \
+            $(OBJDIR)/vorbis_io.o 					      \
+            $(OBJDIR)/header1.o                                               \
+            $(OBJDIR)/header2.o                                               \
+            $(OBJDIR)/common_header.o                                               \
+    	    $(OBJDIR)/header3.o                                               \
             $(OBJDIR)/residue.o                                               \
             $(OBJDIR)/helpers.o                                               \
             $(OBJDIR)/mapping.o                                               \
             $(OBJDIR)/mode.o                                                  \
             $(OBJDIR)/codebook.o                                              \
             $(OBJDIR)/codebook_read.o                                         \
-            $(OBJDIR)/time_domain_transform.o                                 \
             $(OBJDIR)/envelope.o                                              \
-	    $(OBJDIR)/dot_product.o                                           \
+	    $(OBJDIR)/dot_product.o                                           
 
-MY_OBJECTS = $(OBJ)/vorbis_headers.o	
+MY_OBJECTS = $(OBJ)/time_domain.o	
 
 
 quiet-command = $(if $(VERB),$1,$(if $(2),@echo $2 && $1, @$1))
 
 all     : $(OBJDIR) $(PROG)
 
-$(MY_OBJECTS): src/vorbis_headers.c $(INCDIR)
+$(MY_OBJECTS): src/time_domain.c $(INCDIR)
 	$(LD) $(CFLAGS) $(LDFLAGS) -c $< -o $@ 
 
 $(PROG) : $(OBJECTS) $(MY_OBJECTS)
---
+++ src/time_domain.c
@@ -0,0 +1,95 @@
+#include "time_domain.h"
+#include <math.h>
+#include "sample.h"
+#include "vorbis_io.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+struct time_domain_transform1 {
+
+	time_domain_transform_t base;
+	uint16_t *blocksize;
+};
+
+typedef struct time_domain_transform1 time_domain_transform1_t;
+
+status_t time_domain_transforms_setup_init(vorbis_stream_t *stream,
+					   time_domain_transform_t **ptdt){
+	
+	//forward in the flux
+	status_t stat;
+	uint32_t p_count, dst;
+
+	stat = vorbis_read_nbits(6, &dst, stream->io_desc, &p_count);
+	if ((stat==VBS_BADSTREAM) || (p_count != 6)) return VBS_BADSTREAM;
+	
+	
+	uint32_t time_count=(uint32_t)dst;
+	time_count ++;
+
+
+	for (uint32_t i=0; i<time_count; i++){
+			
+		stat = vorbis_read_nbits(16, &dst, stream->io_desc, &p_count);
+		if ((stat==VBS_BADSTREAM) || (p_count != 16) 
+				|| (dst != 0)) return VBS_BADSTREAM;
+	}	
+
+	//creation of structure time_domain_transform_t
+	uint16_t *block=calloc(2,sizeof(uint16_t));
+	
+	for (uint32_t i=0; i<2; i++){
+		block[i]=(stream->codec->blocksize)[i];
+	}
+	
+	time_domain_transform1_t *ptdt1=malloc(sizeof(time_domain_transform1_t));
+	ptdt1->base.type=0;
+	ptdt1->blocksize=block;
+	*ptdt=(time_domain_transform_t *)(ptdt1);
+
+	printf("\n init time_domain done \n");
+        return VBS_SUCCESS;	
+}
+
+
+void time_domain_transforms_free(time_domain_transform_t *tdt){
+
+	time_domain_transform1_t *tdt1=(time_domain_transform1_t *) tdt;
+	free(tdt1->blocksize);
+	free(tdt1);
+	printf("free time_domain done \n");
+}
+
+/*Needed:
+ * -the filter must be initialized by the envelop module
+ * -the size of all the sample_t * must be initialized, calloc made before for instance */
+status_t time_domain_transform_process(time_domain_transform_t *tdt, sample_t *fsamp,
+				       sample_t *tsamp, sample_t *filter, int mode){
+	
+	time_domain_transform1_t *tdt1=(time_domain_transform1_t *)tdt;
+	uint32_t n=(tdt1->blocksize)[mode];
+	
+	sample_t temp, aux;
+	//search if there is no possibilies to accelerate the process
+
+	for (uint32_t i=0; i<n; i++){
+		temp=(sample_t)0;
+		for (uint32_t j=0; j<(n/2); j++){
+			//Calculating the j element of the sum
+			aux  =(sample_t)(2*j+1);
+			aux *=(sample_t)(2*i +1 +(n/2));
+			aux /=(sample_t)n;
+			aux *=M_PI_2;
+			aux =cos(aux);
+			aux *=fsamp[j];
+			//add aux to the previous sum 
+			temp +=aux;
+		}
+		temp *=filter[i]*filter[i];
+		tsamp[i]=temp;
+	}
+	return VBS_SUCCESS;	
+}
+
