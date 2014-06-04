6c6ac50eb311d566d4740586e8d76fe54f7b2e84
ensimag <ensimag@ensilinux.(none)>
2014-06-04 17:22:46 +0200

    ok

--- src/envelope.c
+++ src/envelope.c
@@ -3,10 +3,15 @@
 
 envelope_t *envelope_init(uint16_t *blocksize){
 
+	//reading in the flux
+	
+	//initialization
+	
 }
 
 void envelope_free(envelope_t *env){
-
+	free(env->blocksize);
+	free(env);
 }
 
 status_t envelope_prepare(envelope_t *env, sample_t *filter){
@@ -16,4 +21,44 @@ status_t envelope_prepare(envelope_t *env, sample_t *filter){
 uint16_t envelope_overlap_add(envelope_t *env, sample_t *in,
                               sample_t *cache, sample_t *out){
 
+	if (!(env->initialized)){
+	//we are at the beginning
+	
+	uint32_t nc=(env->blocksize)[env->curr_window]/2;
+	//nc vaut la taille de la fenetre Courante divisée par 2
+	sample_t *out=malloc(sizeof(sample_t)*nc);
+	sample_t *cache=malloc(sizeof(sample_t)*nc);
+	for (uint32_t i=0; i<nc;i++){
+	
+	}
+
+	
+	
+	}else{
+	//we are not at the beginning
+	
+
+
+	}
+
+	uint32_t nc=
+	uint32_t np=
+	
+	
+
+	//add the first part of the current window
+	for (uint32_t i=0; i<
+	
+	//add the second part of the previous if it exists
+	else{
+		for (uint32_t i=0
+	}
+	//mettre la condition voir si on est à la premiere fenetre ou non
+
+	//preparing the cache
+	free(cache); //est ce que ce garde l adresse ?
+	//on sinon on fait un cache de la taille de la plus grande fenetre
+
+
+	
 }
