0860cf6ca2cdfd9fcc3b409d20179dc76dfe498b
ensimag <ensimag@ensilinux.(none)>
2014-06-09 23:23:38 +0200

    bin and little_endian added

--- src/pcm_handler.c
+++ src/pcm_handler.c
@@ -5,7 +5,10 @@
 
 
 #define NB_FORMAT 2
+
 //demander à bap comment on free un pointeur de fonction
+typedef struct pcm_handler1 pcm_handler1_t;
+
 
 struct pcm_handler1 {
 
@@ -16,7 +19,6 @@ struct pcm_handler1 {
 	const char *arg;
 };
 
-typedef struct pcm_handler1 pcm_handler1_t;
 
 void pcm_handler_list(const char *prefix){
 	printf("entree pcm_handler_list \n");
@@ -40,43 +42,65 @@ int init(pcm_handler_t * hdlr, unsigned int sampl,
 	return 0;
 };
 
+//Fonction qui permet de passer en little_endian des int16_t
+int16_t swap_int16( int16_t val ) 
+{
+    //return (val << 8) | ((val >> 8) & 0xFF);
+    return ((val & 0xFF) << 8) | (val >> 8);
+}
+
+//Fonction qui permet de convertir un entier en un string 
+//correspondant à sa valeur binaire//fonctionne pas, j ai besoin des 0 inutiles
+//aussi
+char * bin (unsigned long int i)
+{
+    static char buffer [1+sizeof (unsigned long int)*8] = { 0 };
+    char *p=buffer-1+sizeof (unsigned long int)*8;
+    do { *--p = '0' + (i & 1); i >>= 1; } while (i);
+    return p;
+}
+
+//permet d'afficher en binaire little endian un string
+void affichage_string(char *s){
+	printf("%s",bin((unsigned long int)s));
+}
+
+void affichage_entier(uint8_t u){
+	printf("%s",bin(u));
+}
+
+
 int process_raw (pcm_handler_t * hdlr, unsigned int num,int16_t ** samples){
 	
-	printf("entree dans process_raw \n");	
-	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
-	for (uint32_t n; n<num; n++){
-		for (uint32_t ch=0; ch<(hdlr1->nchan); ch ++){
-			printf("%d",(samples[ch])[n]);
-			//afficher d'une autre facon surement	
-		}
-		printf("passage à la ligne \n");
-	}
-	return 0;
 }
 
 int process_wav (pcm_handler_t * hdlr, unsigned int num,
 			int16_t ** samples) {
 
-	printf("entree dans process_wav \n");
+	//printf("entree dans process_wav \n");
 	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
 	FILE *fp=fopen(hdlr1->arg,"w");
 
+
 	//bloc de declaration d'un fichier wav
+
 
 
 	//bloc decrivant le format audio
 
 
 	//bloc des données
+	
 
 	//le mieux serait ici d'appeller process_raw pas possible vu que ca renvoie 0
 	
 	for (uint32_t n=0; n<num; n++){
 		for (uint32_t ch=0; ch<(hdlr1->nchan); ch ++){
-			//printf("%d",(samples[ch])[n]);
-			//afficher d'une autre facon surement	
+		//	printf("%s ",bin((samples[ch])[n]));
+		//	printf("%s ",bin(swap_int16((samples[ch])[n])));
+		//	printf("2 affichages\n");
+			//il faut afficher en little endian	
 		}
-		//printf("passage à la ligne \n");
 	}
 	return 0;
 
@@ -120,7 +144,12 @@ pcm_handler_t *pcm_handler_create(const char *format, const char *arg){
 		else{
 			hdlr1->base.process=process_raw;
 		}
-			
+		//printing the first parameters
+		
+		FILE *fp=fopen(hdlr1->arg,"w");
+		affichage_string("RIFF");
+
+
 		printf("end handler create \n");
 		return (pcm_handler_t *)hdlr1;
 	}
