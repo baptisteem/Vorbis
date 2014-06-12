5eccd91f28845739de1cbc4cb239921e53bcd997
ensimag <ensimag@ensilinux.(none)>
2014-06-12 14:45:27 +0200

    compiling and no leaks, starting debeuging

--- src/pcm_handler.c
+++ src/pcm_handler.c
@@ -7,7 +7,6 @@
 #define NB_FORMAT 2
 
 //demander à bap comment on free un pointeur de fonction, le mettre À null il ne 
-//prend pas de place
 //trouver une fonction qui affiche en binaire sans perte à l'avant ou arriere
 //trouver une fonction qui permet de donner la conversion en little endian
 //tenter de regrouper les fonctions raw 
@@ -23,6 +22,8 @@ struct pcm_handler1 {
 	unsigned int nchan;
 	const char *format;
 	const char *arg;
+	int32_t size;
+	FILE *fp;
 };
 
 
@@ -36,131 +37,152 @@ void pcm_handler_list(const char *prefix){
 	}
 }
 
-int init(pcm_handler_t * hdlr, unsigned int sampl,
-		     unsigned int nchan){
-
-	printf("entree dans init \n");
-	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
-	hdlr1->nchan=nchan;
-	hdlr1->sampl=sampl;
-	//cette fonction doit etre utilisée une seule fois
-	hdlr1->base.init=NULL;
-
-	//bloc de declaration d'un fichier wav
-			
-	affichage_string("RIFF");
-
-
-	//bloc decrivant le format audio
-
-	return (pcm_handler_t *)hdlr1;
-	
-	return 0;
-};
-
-//Fonction qui permet de passer en little_endian des int16_t
-int16_t swap_int16( int16_t val ) 
-{
-    //return (val << 8) | ((val >> 8) & 0xFF);
-    return ((val & 0xFF) << 8) | (val >> 8);
-}
-
-
-void print_little_endian (uint32_t size,uint32_t n){
-	//size peut valoir 16 ou 32 (2 ou 4 octets à afficher)
-	
-	char c[size];
-	for (uint32_t i=0; i<size; i++){
-		c[i]=n % mod 2;
-		c /= 2;
-	}
-
-
-}
-
-
-
-
-int process_raw (pcm_handler_t * hdlr, unsigned int num,int16_t ** samples){
-	
-}
-
-int process_wav (pcm_handler_t * hdlr, unsigned int num,
-			int16_t ** samples) {
-
-	//printf("entree dans process_wav \n");
-	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
-	FILE *fp=fopen(hdlr1->arg,"w");
-
-	//bloc des données
-	
-
-	//le mieux serait ici d'appeller process_raw pas possible vu que ca renvoie 0
-	
-	for (uint32_t n=0; n<num; n++){
-		for (uint32_t ch=0; ch<(hdlr1->nchan); ch ++){
-		//	printf("%s ",bin((samples[ch])[n]));
-		//	printf("%s ",bin(swap_int16((samples[ch])[n])));
-		//	printf("2 affichages\n");
-			//il faut afficher en little endian	
-		}
-	}
-
-	
-	return 0;
-
-}
-
-
-int finalize (pcm_handler_t * hdlr) {
-
-	printf("entree finalize \n");
-	// on empeche la réutilisation de la fonction process
-	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
-	hdlr1->base.process=NULL;
-	return 0;
-}
-
+int init_wav(pcm_handler_t * hdlr, unsigned int sampl,unsigned int nchan);
+int init_raw(pcm_handler_t * hdlr, unsigned int sampl,unsigned int nchan);
+int process (pcm_handler_t * hdlr, unsigned int num,int16_t ** samples);
+int finalize_wav (pcm_handler_t * hdlr);
+int finalize_raw (pcm_handler_t * hdlr);
 
 pcm_handler_t *pcm_handler_create(const char *format, const char *arg){
 	
-	printf("debut de handler_create %s \n",format);
-
 	if (strcmp(format,"wav") && strcmp(format,"raw")){
-	//realiser le test plutot avec la fct d affichage
 		
 			printf("mauvais format \n");
 			return (pcm_handler_t *)NULL;
 	}
 	else{
-		printf("entree dans initialization \n");	
 		//initializing the parameters
-	
 		pcm_handler1_t *hdlr1=malloc(sizeof(pcm_handler1_t));
 
 		hdlr1->format=format;
 		hdlr1->arg=arg;
-		hdlr1->base.init=init;
-		hdlr1->base.finalize=finalize;
+		hdlr1->size=0;
+		hdlr1->base.process=process;
+		hdlr1->fp=fopen(hdlr1->arg,"wb");
+
 
 		if (!strcmp(format,"raw")){
 
-			hdlr1->base.process=process_raw;
+			hdlr1->base.init=init_raw;
+			hdlr1->base.finalize=finalize_raw;
 			return (pcm_handler_t *)hdlr1;
 		}
 		else{
-			hdlr1->base.process=process_wav;
+			hdlr1->base.init=init_wav;
+			hdlr1->base.finalize=finalize_wav;
+			return (pcm_handler_t *)hdlr1;
 		}
+
+
 	}
 
 }
 
+
+/*************************************************************/
+
+int init_raw(pcm_handler_t * hdlr, unsigned int sampl,
+		     unsigned int nchan){
+	hdlr->init=NULL;
+}
+
+
+int init_wav(pcm_handler_t * hdlr, unsigned int sampl,
+		     unsigned int nchan){
+
+	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
+	hdlr1->nchan=nchan;
+	hdlr1->sampl=sampl;
+	hdlr1->base.init=NULL;
+ 	//seek  
+
+	uint32_t p;
+	p=0x52494646;//"RIFF" en bigendian	
+	fwrite(&p,4,1,hdlr1->fp);
+	p=0;//on reserve juste la place necessaire pour la suite
+	fwrite(&p,4,1,hdlr1->fp);
+	p=0x57415645;//"WAVE" en bigendian
+	fwrite(&p,4,1,hdlr1->fp);
+	hdlr1->size +=12;
+
+	//bloc decrivant le format audio
+	p=0x666d7420; //"fmt " en bigendian
+	fwrite(&p,4,1,hdlr1->fp);
+	p=16;
+	fwrite(&p,4,1,hdlr1->fp);
+	p=1;
+	fwrite(&p,2,1,hdlr1->fp);
+	p=hdlr1->nchan;
+	fwrite(&p,2,1,hdlr1->fp);
+	p=hdlr1->sampl;
+	fwrite(&p,4,1,hdlr1->fp);
+	p=2* hdlr1->sampl * hdlr1->nchan;
+	fwrite(&p,4,1,hdlr1->fp);
+	p=2*hdlr1->nchan;
+	fwrite(&p,2,1,hdlr1->fp);
+	p=16;
+	fwrite(&p,2,1,hdlr1->fp);
+	hdlr1->size +=24;
+		
+
+	//bloc donnant les échantillons
+	p=0x64617461; //"data" en bigendian
+	fwrite(&p,4,1,hdlr1->fp);
+	p=0; //on reserve juste la place necessaire pour la suite
+	fwrite(&p,4,1,hdlr1->fp);
+	hdlr1->size +=8;
+
+	return 0;
+
+};
+
+/**************************************************************/
+
+int process (pcm_handler_t * hdlr, unsigned int num,
+			int16_t ** samples) {
+
+	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
+	int16_t p;
+	
+	for (uint32_t n=0; n<num; n++){
+		for (uint32_t ch=0; ch<(hdlr1->nchan); ch ++){
+			
+		p=(samples[ch])[n];
+		fwrite(&p,2,1,hdlr1->fp);
+		hdlr1->size += 2;
+		}
+	}	
+	return 0;
+}
+
+int finalize_raw (pcm_handler_t * hdlr) {
+
+	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
+	hdlr1->base.process=NULL; 
+  	fclose(hdlr1->fp);
+
+	return 0;
+}
+
+int finalize_wav (pcm_handler_t * hdlr) {
+
+	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
+	uint32_t p;
+	fseek(hdlr1->fp,4,SEEK_SET);
+	p=hdlr1->size-8;
+	fwrite(&p,4,1,hdlr1->fp);
+	fseek(hdlr1->fp,40,SEEK_SET);
+	p=hdlr1->size-44;
+	fwrite(&p,4,1,hdlr1->fp);
+
+	hdlr1->base.process=NULL;
+	fclose(hdlr1->fp);
+};
+
+
+
 void pcm_handler_delete(pcm_handler_t * hdlr){
 
-	printf("entree delete \n");
 	pcm_handler1_t *hdlr1=(pcm_handler1_t *)hdlr;
-	//free(hdlr1->base.init);
-	//free(hdlr1->base.process);
-	//free(hdlr1->base.finalize);
 	free(hdlr1);
 }
