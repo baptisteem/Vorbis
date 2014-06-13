5e8037c2f90e9a8e1fa57029d0a1c4f3bc5cf26e
Baptiste EMMANUEL <baptiste.em@gmail.com>
2014-06-13 11:55:08 +0200

    Fix main issue. Add -s option

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 
 void display_list_stream(ogg_physical_stream_t *ogg_stream);
 void display_help();
+ogg_status_t decode_ogg_id_stream(long id, ogg_physical_stream_t *ogg_stream, pcm_handler_t *pcm);
 
 int main(int argc, char *argv[]) {
 
@@ -19,17 +20,19 @@ int main(int argc, char *argv[]) {
   long id = -1;
   char *outformat = "wav";
   char outfile[255] = "\0";
+  ogg_status_t ret;
 
   while( (c = getopt(argc,argv,format)) != -1){
     switch(c){
       case 'h':
         display_help();
+        return 0;
         break;
       case 'l':
         list_stream = 1;
         break;
       case 's':
-        id = (long) optarg;
+        id = (long) atoi(optarg);
         break;
       case 'f':
         outformat = optarg;
@@ -58,27 +61,66 @@ int main(int argc, char *argv[]) {
 
     ogg_physical_stream_t *physical_stream = NULL;
 
-    if(ogg_init(file,&physical_stream) == OGG_OK){
-
-      //If we want to list all logical streams
-      if(list_stream==1)
-        display_list_stream(physical_stream);
-
-      pcm_handler_t *pcm_handler = pcm_handler_create(outformat,outfile);
-
-      ogg_decode(physical_stream->first, pcm_handler);
-
-      pcm_handler_delete(pcm_handler);
-      ogg_term(physical_stream);
-    }
-    else
+    ret = ogg_init(file,&physical_stream);
+    if(ret != OGG_OK){
       printf("Aucun flux physique disponible.\n");
+      return 0;
+    }
 
+    //If we want to list all logical streams
+    if(list_stream==1){
+      display_list_stream(physical_stream);
+      return 0;
+    }
+
+    pcm_handler_t *pcm_handler = pcm_handler_create(outformat,outfile);
+
+    //If we don't ask for a specific stream to decode
+    if(id == -1){
+      ret = ogg_decode(physical_stream->first, pcm_handler);
+      if(ret != OGG_OK)
+        return ret;
+    }
+    else{
+      ret = decode_ogg_id_stream(id, physical_stream, pcm_handler);
+      if(ret != OGG_OK)
+        return ret;
+    }
+
+    pcm_handler_delete(pcm_handler);
+
+    ret = ogg_term(physical_stream);
+    if(ret != OGG_OK)
+      return ret;
   }
   else
     printf("Failed to open input file '%s': No such file or directory\n", argv[1]);
 
   return 0;
+}
+
+ogg_status_t decode_ogg_id_stream(long id, ogg_physical_stream_t *ogg_stream, pcm_handler_t *pcm){
+
+  ogg_status_t ret;
+  ogg_logical_stream_t *tmp = ogg_stream->first;
+
+  //We are searching for the appropriate logical stream
+  while(tmp != NULL){
+
+    if(tmp->stream_id == id){
+
+      ret = ogg_decode(ogg_stream->first, pcm);
+      if(ret != OGG_OK)
+        return ret;
+
+      return OGG_OK;
+    }
+    tmp = tmp->next;
+  }
+
+  printf("Stream with id %ld not found\n", id);
+
+  return OGG_ERR_ILL_OP;
 }
 
 /* Display list of all logical stream in a physical stream */
@@ -88,8 +130,8 @@ void display_list_stream(ogg_physical_stream_t *ogg_stream){
   ogg_logical_stream_t *tmp = ogg_stream->first;
 
   printf("%d logical stream(s):\n", ogg_stream->nb_streams);
-  while(tmp != NULL)
-  {
+  while(tmp != NULL){
+
     if(tmp->codec == OGG_VORBIS)
       printf("\t%d: codec='vorbis', id=%d\n", cpt, tmp->stream_id);
     else
