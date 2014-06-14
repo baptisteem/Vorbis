c4bf4691388e905a5f1298f97df8d49ac4ddf377
Baptiste EMMANUEL <baptiste.em@gmail.com>
2014-06-14 11:24:14 +0200

    Commit before changing branch

--- src/ogg_core.c
+++ src/ogg_core.c
@@ -2,7 +2,15 @@
 
 ogg_status_t ogg_init(FILE* file, ogg_physical_stream_t **ppstream){
 
+  internal_ogg_logical_stream_t *l_stream = malloc(sizeof(internal_ogg_logical_stream_t));  
+
   
 
   return OGG_OK;
+}
+
+ogg_status_t ogg_get_next_page(internal_ogg_logical_stream_t *lstream){
+
+  
+
 }
